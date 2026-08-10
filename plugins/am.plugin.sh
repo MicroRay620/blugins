ace13e50c0f3ac9bc4319cee914f61e4977fba7e
Ruby Rose <microray620@gmail.com>
2026-08-10 16:17:31 -0400

    feat: log file integration

--- plugins/am.plugin.sh
+++ plugins/am.plugin.sh
@@ -7,36 +7,29 @@
 # description = This is a plugin for the AM/AppMan appimage package manager
 # source = https://codeberg.org/RubyRose/blugins/raw/branch/main/plugins/am.plugin.sh
 # shellcheck source=/dev/null 
-source -- "${XDG_DATA_HOME:-$HOME/.local/share}/banager/src/declare"
-am_list=( "am" "appman" )
-grabs=( "curl" "wget" )
-for app in "${am_list[@]}"; do 
-    if $app &>/dev/null; then
-        working="true"
-    else
-        working="false"
-    fi
-done
-for grab in "${grabs[@]}"; do 
-    if command -v "$grab" &>/dev/null; then 
-        grabber="$grab" 
-    fi
-done
-if [ "$grabber" = "curl" ]; then 
-    grabber="curl -sLO"
+source -- "${XDG_DATA_HOME:-$HOME/.local/share}/banager/src/declare.sh"
+if command -v am &>/dev/null || command -v appman &>/dev/null; then 
+    working=true 
+else 
+    working=false
+fi
+if command -v curl &>/dev/null; then 
+    grabber="curl -sLo"
     get_cmd="https://raw.githubusercontent.com/ivan-hc/AM/main/AM-INSTALLER"
-elif [ "$grabber" = "wget" ]; then 
+elif command -v wget &>/dev/null; then 
     grabber="wget -q"
     get_cmd="./AM-INSTALLER https://raw.githubusercontent.com/ivan-hc/AM/main/AM-INSTALLER"
 fi
-if [ ! -e "${XDG_CACHE_HOME:-$HOME/.cache}/banager/user/app.location.sh" ]; then 
-    touch "${XDG_CACHE_HOME:-$HOME/.cache}/banager/user/app.location.sh"
+if [ ! -e "${XDG_CACHE_HOME:-$HOME/.cache}/banager/plugins/app.location.sh" ]; then 
+    touch "${XDG_CACHE_HOME:-$HOME/.cache}/banager/plugins/app.location.sh"
     # shellcheck disable=SC2154
     echo -e "$env_allow\nWhere do you want to install user applications? "
     read -r app_locale
     # shellcheck disable=SC2154
     echo -e "$bash_declare\n$bash_gen\n$dont_delete\napp_locate=$app_locale" >> "${XDG_CACHE_HOME:-$HOME/.cache}/banager/user/app.location.sh"
 fi
+# shellcheck disable=SC2154
+echo "${XDG_CACHE_HOME:-$HOME/.cache}/banager/plugins/app.location.sh: app_locale is $app_locale" >> "$log_file"
 if [ "$working" = "false" ]; then 
     $grabber "$get_cmd" && chmod a+x ./AM-INSTALLER && ./AM-INSTALLER && rm ./AM-INSTALLER
 fi
--- plugins/archlinux.plugin.sh
+++ plugins/archlinux.plugin.sh
@@ -17,43 +17,34 @@ else
     touch "$XDG_CACHE_HOME/banager/aur.command.sh"
     source -- "$XDG_CACHE_HOME/banager/aur.command.sh"
 fi
-aur=("yay" "paru")
-aur_num=0
 # shellcheck disable=SC2154
 if [ "$PKG_MGR" = "${managers[3]}" ]; then
-    if [ -z "$cache_aur" ] || [ ! "$cache_aur" = "chaotic-aur" ]; then
-        for aur_mgr in "${aur[@]}"; do 
-            if command -v "$aur_mgr" &>/dev/null; then
-                export AUR="$aur_mgr"
-                break
-            else
-                (( aur_num += 1 ))
-                continue
-            fi
-            if [ "$aur_num" -ge 2 ]; then 
-                echo -e "\e[31mERROR: No AUR manager found. \n Cause: BC1: paru or yay does not exist on the system"
-                echo -e "Would you like [paru] or [yay]? (Defaults to paru) [type chaotic to skip] "
-                read -er aur_choice 
-                case "$aur_choice" in 
-                    *yay* | *Yay* | *YAY*) aur_choice="yay" ;;
-                    *paru* | *Paru* | *PARU*) aur_choice="paru" ;;
-                    *chaotic* | *) aur_choice="pacman" ;;
-                esac
-                break   
-            fi
-        done
+    if [ -z "$cache_aur" ] || [ ! "$cache_aur" = "chaotic" ]; then
+        if command -v yay &>/dev/null; then 
+            AUR=yay 
+        elif command -v paru &>/dev/null;then 
+            AUR=paru
+        else 
+            echo -e "\e[31mERROR: No AUR manager found. \n Cause: BC1: paru or yay does not exist on the system"
+            echo -e "Would you like [paru] or [yay]? (Defaults to paru) [type chaotic to skip] "
+            read -er aur_choice 
+            case "$aur_choice" in 
+                *yay* | *Yay* | *YAY*) aur_choice="yay" ;;
+                *paru* | *Paru* | *PARU*) aur_choice="paru" ;;
+                *chaotic* | *) aur_choice="pacman" ;;
+            esac
+        fi
         if [ ! "$aur_choice" = "pacman" ]; then
             $SUPER "${managers[3]}" -S --needed base-devel 
             git clone "https://aur.archlinux.org/${aur_choice}.git"
             cd "$aur_choice" || exit 1
             makepkg -si
         fi
-
         AUR="$aur_choice"
         cached_aur="$aur_choice"
         echo -e "$bash_declare\n$bash_gen\ncached_aur=$cached_aur" >> "$XDG_CACHE_HOME/banager/aur.command.sh"
     fi
-    if [ "$cache_aur" = "chaotic" ]; then
+    if [ "$cached_aur" = "chaotic" ]; then
         chaotic=$(grep "Include = /etc/pacman.d/chaotic-mirrorlist" /etc/pacman.conf)
         if [ -z "$chaotic" ]; then 
             $SUPER pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
@@ -65,6 +56,7 @@ if [ "$PKG_MGR" = "${managers[3]}" ]; then
     else 
         AUR="$SUPER pacman"
     fi
+    echo "${XDG_CONFIG_HOME:-$HOME/.config}/banager/plugins/archlinux.plugin.sh: Cached AUR is $cached_aur"
     if command -v ble &>/dev/null; then 
         $AUR blesh
     fi
