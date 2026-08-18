420d220b709affb282c68a40a4be99d95f37f55a
Ruby Rose <microray620@gmail.com>
2026-08-18 11:38:38 -0400

    feat: optimized the run time by using if instead of for

--- plugins/replacement.plugin.sh
+++ plugins/replacement.plugin.sh
@@ -2,61 +2,33 @@
 #!banager/plugin
 # name = Replacement Commands 
 # owner = RubyRose
-# license = <LICENSE OF THE CODE>
+# license = CC BY-SA 4.0 International
 # description = A plugin to allow you to easily just have the commands be something findable
 # source = https://codeberg.org/RubyRose/banager/raw/branch/main/plugins/nixos.plugin.sh
-grabs=("rg" "rga" "grep")
-for grab in "${grabs[@]}"; do
-    if command -v "$grab" &>/dev/null; then 
-        alias grep='$grab'
-        break 
-    else 
-        continue
-    fi
-done
-dus=("dust" "du")
-du_flags="-sh"
-du_num=0
-for dun in "${dus[@]}"; do 
-    if command -v "$dun" &>/dev/null; then
-        if [ "$du_num" = 0 ]; then
-            dun="$dun $du_flags"
-        fi
-        alias du='$dun'
-        break
-    else 
-        (( du_num += 1 )) 
-    fi
-done
-fd=( "fd" "find" )
-tags=("-tf -tl --show-errors --hidden --prune --exclude '.git node_modules'" "-L")
-fd_num=0
-for look in "${fd[@]}"; do 
-    if command -v "$look" &>/dev/null; then 
-        alias find='$look ${tags[$fd_num]}'
-        break 
-    else 
-        (( fd_num += 1 ))
-        continue
-    fi
-done
-shows=("bat" "cat")
-for show in "${shows[@]}"; do 
-    if command -v "$show" &>/dev/null; then 
-        alias cat='$show'
-        break
-    fi
-done
-ls_list=("eza" "ls")
-ls_flags=("-Gumn --all --no-permissions --no-quotes --icons=always --group-directories-first" "-ChRskNp --all --time=access")
-ls_num=0
-for list in "${ls_list[@]}"; do
-    if command -v "$list" &>/dev/null; then 
-        alias ls='$list ${ls_flags[$ls_num]}'
-        break
-    else 
-        (( ls_num += 1 ))
-        continue
-    fi
-done
+if command -v rg &>/dev/null; then 
+    alias grep=rg
+elif command -v rga &>/dev/null; then 
+    alias grep=rga 
+fi
 
+if ! command -v dust &>/dev/null; then 
+    alias du="du -sh"
+fi
+
+if command -v fd &>/dev/null; then 
+    alias find="fd -tf -tl --show-errors --hidden --prune --exclude '.git node_modules'"
+    alias fd="fd -tf -tl --show-errors --hidden --prune --exclude '.git node_modules'"
+elif command -v find &>/dev/null; then 
+    alias find="find -L"
+fi
+
+if command -v bat &>/dev/null; then 
+    alias cat=bat 
+fi
+
+if command -v eza &>/dev/null; then 
+    alias ls="eza -Gumn --all --no-permissions --no-quotes --icons=always --group-directories-first"
+elif command -v ls &>/dev/null; then
+    alias ls="ls -ChRskNp --all --time=access"
+fi
+alias please='$SUPER $(fc -ln -1)'
